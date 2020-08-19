772ef894fe18a41277ee65e5bf3c80fbbfce64e1
Amar1921 <57800962+Amar1921@users.noreply.github.com>
2020-08-19 23:32:15 +0200

    ajout date

--- script/index.js
+++ script/index.js
@@ -1,6 +1,3 @@
-const dat = document.getElementById('date')
-const date = new Date();
-dat.innerHTML = date.getFullYear()
-$('.carousel').carousel({
-    interval: 3000
-})
+let date = new Date().getFullYear();
+console.log(date)
+document.getElementById("date").innerHTML = date;
